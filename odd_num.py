ca00228c13ef2ce87f403dc5f8829d804a569950
Great-Szu <mateusz.chol.net@outlook.com>
2025-10-01 22:32:35 +0200

    Day 2 Life examples of using generators

---
+++ odd_num.py
@@ -0,0 +1,20 @@
+def odd_numbers():
+    current = 1
+    while True:
+        yield current
+        current = current + 2
+
+
+def pi_series():
+    odds = odd_numbers()
+    approximation = 0
+    while True:
+        approximation += (4 / next(odds))
+        yield approximation
+        approximation -= (4 / next(odds))
+        yield approximation
+
+approx_pi = pi_series()
+
+for x in range(100):
+    print(next(approx_pi))
