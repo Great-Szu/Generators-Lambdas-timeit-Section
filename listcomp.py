95a3b4751eedbfac5d698dcbf5d8f56ffa2633d2
Great-Szu <mateusz.chol.net@outlook.com>
2025-10-11 13:15:18 +0200

    Day 6 Advantages the comprehension has over the for loop and challanges

--- listcomp.py
+++ listcomp.py
@@ -2,6 +2,9 @@ print(__file__)
 
 numbers = [1, 2, 3, 4, 5, 6]
 
-# squares = [number ** 2 for number in numbers]
-squares = [number ** 2 for number in range(1, 7)]
-print(squares)
+number = int(input("Please enter a number, and I'll tell you it's square:"))
+
+squares = [number ** 2 for number in numbers]
+
+index_pos = numbers.index(number)
+print(squares[index_pos])
