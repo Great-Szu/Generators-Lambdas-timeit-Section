ec226d5cac8f8946345b29330efef3dc76a4a749
Great-Szu <mateusz.chol.net@outlook.com>
2025-10-29 20:56:06 +0100

    Day 11 any(), all() and reduce() functions

---
+++ map/anyall.py
@@ -0,0 +1,16 @@
+entries = [1, 2, 3, 4, 5]
+
+print("all: {}".format(all(entries)))
+print("any: {}".format(any(entries)))
+
+print("Iterable with a 'False' value")
+entries_with_zero = [1, 2, 0, 4, 5]
+print("all: {}".format(all(entries_with_zero)))
+print("any: {}".format(any(entries_with_zero)))
+
+print("=" * 80)
+name = "Tim"
+if name:
+    print("Hello {}".format(name))
+else:
+    print("Welcome, person with no name")
